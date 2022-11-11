bc2a0b652c166974491b4fce4f6434c329d7c0c2
gabriel rossi <gabrieldesouzarossi123456@gmail.com>
2022-11-11 20:40:32 -0300

    11/11/2022

---
+++ src/dominio/Conteudo.java
@@ -0,0 +1,28 @@
+package dominio;
+
+public abstract class Conteudo {
+    protected static final double XP_PADRAO = 10;
+    private String titulo;
+    private String descricao;
+
+    public abstract double caucular_XP();
+
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    
+}
--- src/dominio/Mentoria.java
+++ src/dominio/Mentoria.java
@@ -2,24 +2,16 @@ package dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo{
+
     private LocalDate data;
     
     
-    public String getTitulo() {
-        return titulo;
+    @Override
+    public double caucular_XP() {
+        return  XP_PADRAO + 20d;
     }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+
     public LocalDate getData() {
         return data;
     }
@@ -27,10 +19,9 @@ public class Mentoria {
         this.data = data;
     }
 
-    
     @Override
     public String toString() {
-        return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+        return "Mentoria [titulo=" + getTitulo()+ ", descricao=" + getDescricao() + ", data=" + data + "]";
     }
 
     
